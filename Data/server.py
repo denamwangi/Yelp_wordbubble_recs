a6e383e11ebc3c582bf63677157a451828295be1
denamwangi <dena.mwangi@gmail.com>
2017-02-13 17:57:28 -0800

    Added routes to server.py with basic form on homepage

--- Data/server.py
+++ Data/server.py
@@ -5,8 +5,8 @@ from jinja2 import StrictUndefined
 from flask import Flask, render_template, redirect, request, flash, session, jsonify, url_for
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import User, Rating, Movie, connect_to_db, db
-
+from model import User, Business, Category, Location,  Attribute, Review, Tip, Keyword, BusinessKeyword, connect_to_db, db
+from model import BusinessCategoryLocation as BCL 
 
 app = Flask(__name__)
 app.secret_key = "ABC"
@@ -17,20 +17,29 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
+    states = db.session.query(BCL.state).group_by(BCL.state).all()
+    print "THIS IS THE OUTPUT FOR states: "
+    print states
 
-    return render_template("homepage.html")
+    return render_template("homepage.html", states=states)
 
 
-@app.route('/bubble_match')
-def index():
+@app.route('/bubble_match', methods=['GET'] )
+def keyword_choice():
     """page to pick keywords of choice."""
+    state = request.args.get("state")
+    category = request.args.get("category")
+    print state
 
-    return render_template("bubble_match.html")
+    return render_template("bubble_match.html", 
+                            state=state, 
+                            category=category)
 
+@app.route('/results')
+def results():
+    """results with personalized recommendations."""
 
-
-
-
+    return render_template("results.html")
 
 
 
