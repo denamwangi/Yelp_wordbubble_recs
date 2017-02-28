3dea83c44ac74376fc4aee0c498025551db461c4
denamwangi <dena.mwangi@gmail.com>
2017-02-28 14:50:45 -0800

    Functional results page with D3 and selected page with map including marker of final selection

--- Project_files/server.py
+++ Project_files/server.py
@@ -8,10 +8,16 @@ from flask.ext.triangle import Triangle
 from model import User, Business, Category, Location,  Attribute, Review, Tip, Keyword, BusinessKeyword, connect_to_db, db
 from model import BusinessCategoryLocation as BCL 
 from random import choice, sample
+import sys
+import os
 app = Flask(__name__)
 app.secret_key = "ABC"
 app.jinja_env.undefined = StrictUndefined
 Triangle(app)
+
+
+google_api_key= os.environ['GOOGLE_MAPS_API_KEY']
+print "GOOGLE KEY", google_api_key
 
 ### ROUTE FOR HOME PAGE. 
 ###             FEATURE: SEARCH BAR - CATEGORY AND LOCATION
@@ -36,20 +42,37 @@ def restaurant_pics():
 
     businesses = db.session.query(Business).\
                                   join(BusinessKeyword, BCL).\
-                                  filter_by(city='Toronto', category='dinner').\
+                                  filter_by(city='Las Vegas').\
                                   group_by(Business.business_id).limit(20).all()
 
-    businesses = sample(businesses, 5)
+    businesses = sample(businesses, 10)
     results={}
     i=0
+    all_lats = []
+    all_lngs = []
     for business in businesses:
         # print "Business keywords: ", business.keywords
         nlp_keywords =[]
         for keyword_object in business.keywords:
-            nlp_keywords.append(keyword_object.keyword)
+            #need each of these to be an object here to go through
+            # D3 using template. Leaving room for including a size
+            # factor from the NLP analysis
+            kw_obj = {"text": keyword_object.keyword,
+                        "size" : "20",
+                        "group": "1"}
+
+
+
+
+
+
+            nlp_keywords.append(kw_obj)
         nlp_summary = business.summary[0].summary
         # print "HEEEERE",
-        i+=1
+        all_lats.append(business.latitude)
+        all_lngs.append(business.longitude)
+        
+ 
         results['option%s' % i] = {"name" : business.name.lower(),
                                     "business_id" : business.business_id,
                                     "latitude" : business.latitude,
@@ -57,7 +80,13 @@ def restaurant_pics():
                                     "stars" : business.stars,
                                     "nlp_keywords" : nlp_keywords,
                                     "nlp_summary" : nlp_summary} 
-    # print "READ MEEEE", results
+        i+=1
+
+        #calculate the center of the map here
+
+    center_lat = sum(all_lats)/len(all_lats)
+    center_lng = sum(all_lngs)/len(all_lngs)
+    print "HEEERE", center_lat, center_lng
     return jsonify(results)
 
 ### ROUTE THAT DISPLAYS THE HTML FOR THE RESULTS PAGE. FOR NOW ITS
@@ -70,28 +99,55 @@ def keyword_choice():
     """Displays search results of restaurant search."""
 
 
-    return render_template("results.html")
+    return render_template("results.html", google_api_key = google_api_key)
+
+# @app.route('/selected.json')
+# def selected_json():
+#     """Selected restaurant json with helpful information """
+#     final_pick={}
+#     form_results=request.args.get.im_self
+#     for each_chosen in form_results.keys():
+#         business_id=form_results[each_chosen]
+#     business = db.session.query(Business).\
+#                                   join(BCL).\
+#                                   filter_by(business_id=business_id).\
+#                                   group_by(Business.business_id).first()
+    
+#     final_pick['business_id']=business_id
+#     final_pick['name']=business.name
+#     final_pick['latitude']=business.latitude
+#     final_pick['longitude']=business.longitude
+#     final_pick['stars']=business.stars
+#     redirect('/selected')
+#     return jsonify(final_pick)
+
+    # return render_template("selected.html", final_pick = final_pick)
 
 @app.route('/selected')
 def results():
     """Selected restaurant."""
-
+    final_pick={}
     form_results=request.args.get.im_self
-
+    # print form_results
     for each_chosen in form_results.keys():
-        print 'yay', dir(form_results[each_chosen])
-        final_pick=form_results[each_chosen]
-        print 'yay you picked', final_pick
+        business_id=form_results[each_chosen]
+    business = db.session.query(Business).\
+                                  join(BCL).\
+                                  filter_by(business_id=business_id).\
+                                  group_by(Business.business_id).first()
+    
+    final_pick['business_id']=business_id
+    final_pick['name']=business.name
+    final_pick['latitude']=business.latitude
+    final_pick['longitude']=business.longitude
+    final_pick['stars']=business.stars
+    # redirect('/selected')
+    print google_api_key
+    print final_pick
+    return render_template("selected.html", 
+                            final_pick=final_pick, 
+                            google_api_key = google_api_key)
 
-
-    return render_template("selected.html", final_pick = final_pick)
-
-@app.route('/map_play')
-def map_play():
-    """Displays search results of restaurant search."""
-
-
-    return render_template("test_googlemaps.html")
 
 if __name__ == "__main__":
     app.debug = True
